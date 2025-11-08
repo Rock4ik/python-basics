72020a73cb63c630e54c2a2949bdb1d1129abe0d
Rock4ik <ilnazclass@gmail.com>
2025-11-08 21:48:09 +0300

    Добавил разные файлы где я обучался Python

---
+++ a = 4.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+z = int(input())
+v = int(input())
+
+print("Наименьшее число =" , min(a, b, c, z, v))
+print("Наибольшее число =" , max(a, b, c, z, v))
